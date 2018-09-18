93574e136884053730340d76ec1a224e012611fb
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-09-17 20:04:02 -0700

    next, fix xss

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,30 +6,19 @@ class Chatterbox {
     this.text = 'text';
     this.roomname = 'roomname';
     this.message = 'messagesesee';
-    // this.init();
+//   this.publishText = '';
+//   this.publishUsername = '';
+//   this.publishRoomname = '';
+    // this.createMessage();
+    this.init();
   }
 
   init(){
-    //$(document).ready(function() {
-    // setInterval(function() {
-    // this.fetch();
-    // }, 3000);
-    //});
+    this.listeners();
+    setInterval(function() {
+      app.fetch.call(app);
+    }, 3000);
   }
-
-
-  //THIS WAS INSIDE OF OUR SEND FUNCTION <==============
-  // send(){
-  //   // escape text in form to protect against xss attack
-  //   console.log("you clicked me!!!");
-  //   //
-  //   this.message = $("#msgs").serializeArray().reduce(function(obj, item){
-  //     obj[item.name] = item.value;
-  //     return obj;
-  //   }, {});
-  //   this.message.username = this.username;
-  //   console.log('our message: ', this.message);
-
 
   send(message){
     // escape text in form to protect against xss attack
@@ -51,32 +40,41 @@ class Chatterbox {
 
 
 fetch(){
-  // setInterval(function() {
     $.ajax({
       url: this.server,
       type: 'GET',
-      // data: JSON.stringify(message),
       data: 'order=-createdAt',
       // data: {"order":"-createdAt", "group": "chatroom"},
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message received');
-        console.log('data', data)
+        console.log('our data = ', data);
+        app.renderMessage(data);
       },
       error: function (data) {
         console.error('chatterbox: Failed to receive message', data);
       }
     });
-  // }, 1000);
-  }
+    }
 
   clearMessages() {
     $("#chats").empty();
   }
 
-  renderMessage() {
-    let div = $("<div></div>");
-    $("#chats").append(div);
+  renderMessage(data) {
+   // var data = arguments;
+    console.log('this inside render message = ', this);
+    console.log('our data.results = ', data["results"][0]);
+    //   app.renderMessage.apply(null, data);
+    for (var key in data["results"]){
+        console.log('data[results][key] :', data["results"][key]);
+        console.log('data[results][key][username] :', data["results"][key]["username"]);
+        console.log('data[results][key][text] :', data["results"][key]["text"]);
+      var publishUsername = data["results"][key]["username"];
+      var publishText = data["results"][key]["text"];
+      let snippet = $(`<div class ='publishedMessages'><span class='pubusername'>@${publishUsername}</span>: <span class='pubtext'>${publishText}</span></div>`);
+      $("#chats").append(snippet);
+    };
   }
 
   renderRoom() {
@@ -91,42 +89,42 @@ fetch(){
 
   //CREATE A MESSAGE UPON CLICKING SEND
   createMessage(){
-    //link the form with the send button
-    $("#btn").click(function(){
-      console.log("clicked");
+      console.log("create message is running");
       this.text = $("#textmessage").val();
-      console.log(this.text);
+      console.log('our text = ', this.text);
       this.roomname = $("#roomname").val();
-      console.log(this.roomname);
-    });
+      console.log('our roomname = ', this.roomname);
+      this.message = {
+        username: this.username,
+        text: this.text,
+        roomname: this.roomname
+      };
+      console.log('our message = ', this.message);
+      this.send(this.message);
+    };
+
+    //LISTENERS FUNCTION WITH ACTIONS
+    listeners(){
+      $(document).ready(function() {
+        $("#btn").click(function() {
+        console.log("you clicked your button, whoohooo!!!");
+          app.createMessage.call(app);
+        });
+      });
+    };
+    // console.log("create Message is running");
+    // //link the form with the send button
+
+    //   console.log("clicked");
+
+
     //upon clicking send
       //send the information within the form to the app.send() function;
-    this.message = {
-      username: this.username,
-      text: this.text,
-      roomname: this.roomname
-    };
-    console.log(this.message);
-    this.send(this.message);
-  };
+
+
 
 
   };
 
 const app = new Chatterbox();
 
-
-//add a textBox to html with a button;
-//send contents to parse;
-//escape input before sending;
-//get parse stream;
-//place in ID Chats (<div id="chats"></div>);
-
-//add drop down for rooms;
-//allow users to create their own rooms and enter others;
-//allow users to friend other users;
-//display all messages sent by friends in bold;
-//allow users to create their own name
-//allow users to send messages
-//set up a way to refresh messages either automatically or with a button
-//
