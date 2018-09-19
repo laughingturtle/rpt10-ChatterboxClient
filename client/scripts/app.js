1f13d7572955629d1c7098b1f8f9d63feb1869ca
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-09-18 17:03:12 -0700

    ssx protection added

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,4 @@
-// YOUR CODE HERE:
+ // YOUR CODE HERE:
 class Chatterbox {
   constructor() {
     this.server = "http://parse.rpt.hackreactor.com/chatterbox/classes/messages"
@@ -10,6 +10,7 @@ class Chatterbox {
 //   this.publishUsername = '';
 //   this.publishRoomname = '';
     // this.createMessage();
+    this.rooms = [];
     this.init();
   }
 
@@ -62,18 +63,23 @@ fetch(){
   }
 
   renderMessage(data) {
-   // var data = arguments;
-    console.log('this inside render message = ', this);
-    console.log('our data.results = ', data["results"][0]);
+    // var data = arguments;
+    // console.log('this inside render message = ', this);
+    // console.log('our data.results = ', data["results"][0]);
     //   app.renderMessage.apply(null, data);
     for (var key in data["results"]){
         console.log('data[results][key] :', data["results"][key]);
         console.log('data[results][key][username] :', data["results"][key]["username"]);
         console.log('data[results][key][text] :', data["results"][key]["text"]);
-      var publishUsername = data["results"][key]["username"];
-      var publishText = data["results"][key]["text"];
-      let snippet = $(`<div class ='publishedMessages'><span class='pubusername'>@${publishUsername}</span>: <span class='pubtext'>${publishText}</span></div>`);
-      $("#chats").append(snippet);
+      var publishUsername = this.encodeHTML(data["results"][key]["username"]);
+      var publishText = this.encodeHTML(data["results"][key]["text"]);
+      let snippet = $(`<div class ='publishedMessages'><span class='pubusername'>@${publishUsername}</span>:- <span class='pubtext'>${publishText}</span></div>`);
+      if(publishUsername !== undefined){
+        //XSS METHOD CALLBACK
+      if(publishText.includes("&lt;") !== true && publishText.includes("('") !== true && publishText.includes("$(") !== true){
+          $("#chats").append(snippet);
+        }
+      }
     };
   }
 
@@ -89,17 +95,17 @@ fetch(){
 
   //CREATE A MESSAGE UPON CLICKING SEND
   createMessage(){
-      console.log("create message is running");
+      // console.log("create message is running");
       this.text = $("#textmessage").val();
-      console.log('our text = ', this.text);
+      // console.log('our text = ', this.text);
       this.roomname = $("#roomname").val();
-      console.log('our roomname = ', this.roomname);
+      // console.log('our roomname = ', this.roomname);
       this.message = {
         username: this.username,
         text: this.text,
         roomname: this.roomname
       };
-      console.log('our message = ', this.message);
+      // console.log('our message = ', this.message);
       this.send(this.message);
     };
 
@@ -112,19 +118,50 @@ fetch(){
         });
       });
     };
+
+    //XSS ESCAPE METHOD
+    encodeHTML(data){
+      console.log('ssssssssssssssssss = ', data);
+      if(data !== undefined){
+        return data.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+      }
+    }
     // console.log("create Message is running");
     // //link the form with the send button
-
     //   console.log("clicked");
 
-
     //upon clicking send
-      //send the information within the form to the app.send() function;
-
-
-
+    //send the information within the form to the app.send() function;
 
   };
 
 const app = new Chatterbox();
+/*
+errors
+- code is continually adding the same posts the page, over and over. Is not updating with new ones. - page refresh? or view refresh?
 
+refactor to pass the following tests:
+
+1) chatroom behavior - should be able to clear messages from the DOM.
+2) should try to send a message upon clicking submit.
+ */
+
+/*
+fix fetch / loading so new posts show up and only new posts are posted.
+refresh displayed messages
+
+refactor to pass the following tests:
+
+1) chatroom behavior - should be able to clear messages from the DOM.
+2) should try to send a message upon clicking submit.
+
+refactor code for es6
+
+Add new functionality:
+xss escape / parse data before we post it to the page.
+issue xss attack
+
+-- add rooms as per learn spec
+-- add socializing options as per learn spec
+
+*/
